9a3ac822ea457c45d0da133a2edbcd458587bf44
Marcia Belén Alvarez <alvarezmarciabelen@gmail.com>
2022-06-14 21:50:16 -0300

    Primer archivo

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Proyecto final: "Nombre del proyecto" 
+Materia: Laboratorio de Computacion I
+Profesores:
+            Balmaceda, Javier
+            Coppo, Ricardo
+Autores:
+        Alvarez, Marcia Belen
+        D'Annunzio, Stefano 
+'''
